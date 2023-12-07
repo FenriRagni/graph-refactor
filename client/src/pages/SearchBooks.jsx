44639fcc73be2110109f75d42272624da749a883
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-12-06 22:26:11 -0500

    updated Saved Books and SignUp Form

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -12,7 +12,7 @@ import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations'
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -77,7 +77,7 @@ const SearchBooks = () => {
 
     try {
       const response = await saveBook({
-        variables: {...savedBookIds}
+        variables: { ...bookToSave }
       });
 
       if (!response.ok) {
